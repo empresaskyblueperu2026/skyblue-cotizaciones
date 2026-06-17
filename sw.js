886ea6f4d8db68c51a10a5c7eb82ebb0e30cacc5
diegofernando7810-max <diegofernando7810@gmail.com>
2026-06-17 00:19:42 -0500

    SIGMA: mejorar propuesta y robustecer extraccion de cotizaciones

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* SKY BLUE PWA - Service Worker */
-const CACHE = 'skyblue-v25';
+const CACHE = 'skyblue-v26';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', function(e){
