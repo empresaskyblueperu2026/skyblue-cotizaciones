94785c821c1002e0f2f9d98225eb6b18d9d9980c
diegofernando7810-max <diegofernando7810@gmail.com>
2026-06-25 00:54:25 -0500

    feat: avances modulos SIGMA (dashboard, proyectos, proveedores) + sw cache bump

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* SKY BLUE PWA - Service Worker */
-const CACHE = 'skyblue-v27';
+const CACHE = 'skyblue-v30';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', function(e){
